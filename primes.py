241b30ff4cbf63a7197587186155c6b55cbce568
Sean Kelley <skelley@palantir.com>
2017-09-05 20:41:57 -0400

    Add memoizing Sieve class and unit tests for basic operations.

--- primes.py
+++ primes.py
@@ -2,9 +2,9 @@ from math import sqrt
 
 def is_prime(n):
   if int(n) != n:
-    raise ValueError()
+    raise ValueError('cannot check primality of non-integer value %d' % n)
   elif n <= 0:
-    raise ValueError()
+    raise ValueError('cannot check primality of non-positive value %d' % n)
   elif n == 1:
     return False
   elif n == 2:
@@ -17,3 +17,74 @@ def is_prime(n):
       if n % i == 0:
         return False
     return True
+
+class Sieve:
+  def __init__(self):
+    self.sieve = [2]
+
+  def _is_prime(self, n):
+    return all(n % p != 0 for p in self.sieve)
+
+  def _add_primes_until_value(self, n):
+    candidate = self.sieve[-1] + 1
+    while n > self.sieve[-1]:
+      while not self._is_prime(candidate):
+        candidate += 1
+      self.sieve.append(candidate)
+      candidate += 1
+
+  def _add_primes_until_index(self, i):
+    candidate = self.sieve[-1] + 1
+    while len(self.sieve) <= i:
+      while not self._is_prime(candidate):
+        candidate += 1
+      self.sieve.append(candidate)
+      candidate += 1
+
+  def indexof(self, n):
+    self._add_primes_until_value(n)
+    # TODO: Binary search.
+    for i, p in enumerate(self.sieve):
+      if p >= n:
+        return i
+
+    assert False, 'should always find something greater than or equal to the number given'
+
+  def nth(self, i):
+    self._add_primes_until_index(i)
+    return self.sieve[i]
+
+  def primes(self, *args):
+    if len(args) == 0:
+      start, stop = 2, None
+    elif len(args) == 1:
+      start, stop = 2, args[0]
+    elif len(args) == 2:
+      start, stop = args
+    else:
+      raise ValueError('primes accepts 0-2 arguments, got %d' % len(args))
+
+    if int(start) != start:
+      raise ValueError('start value must be an integer')
+    elif stop is not None and int(stop) != stop:
+      raise ValueError('stop value must be an integer')
+
+    return PrimeIterator(start, stop, self)
+
+class PrimeIterator:
+  def __init__(self, start, stop, sieve):
+    self.start = start
+    self.stop = stop
+    self.sieve = sieve
+
+  def __iter__(self):
+    i = self.sieve.indexof(self.start if self.start > 2 else 2)
+    p = self.sieve.nth(i)
+    end = self.stop if self.stop is not None else float('inf')
+
+    while p < end:
+      yield p
+      i += 1
+      p = self.sieve.nth(i)
+
+    raise StopIteration()
--- test/primes_test.py
+++ test/primes_test.py
@@ -1,5 +1,7 @@
 import unittest
+import itertools
 from primes import is_prime
+from primes import Sieve
 
 class TestPrimes(unittest.TestCase):
 
@@ -17,5 +19,26 @@ class TestPrimes(unittest.TestCase):
     self.assertFalse(is_prime(1))
     self.assertFalse(is_prime(100000))
 
+  def test_sieve_nth(self):
+    sieve = Sieve()
+
+    self.assertEqual(sieve.nth(0), 2)
+    self.assertEqual(sieve.nth(1), 3)
+    self.assertEqual(sieve.nth(10), 31)
+
+  def test_sieve_primes_illegal(self):
+    sieve = Sieve()
+
+    self.assertRaises(ValueError, sieve.primes, 1.1)
+    self.assertRaises(ValueError, sieve.primes, 1, 1.1)
+
+  def test_sieve_primes(self):
+    sieve = Sieve()
+
+    self.assertEqual([2, 3, 5], list(itertools.islice(sieve.primes(), 0, 3)))
+    self.assertEqual([2, 3, 5, 7, 11], list(sieve.primes(12)))
+    self.assertEqual([], list(sieve.primes(4, 3)))
+    self.assertEqual([5, 7, 11], list(sieve.primes(4, 12)))
+
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
