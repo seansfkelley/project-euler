6835291a9f3b70cb7f7617d004bb601aaaf435bc
Sean Kelley <seansfkelley@gmail.com>
2017-09-07 10:24:48 -0700

    Dust off the ol' Haskell and try to get 214 to work.
    I'm fairly sure the memoization is working as expected, but I think the
    algorithm is just unbearably slow.

--- 214.hs
+++ 214.hs
@@ -1,19 +1,26 @@
 import Data.Numbers.Primes
+import Data.Function
+import Data.Function.Memoize
 
+lowestDivisor :: Int -> [Int] -> Int
+lowestDivisor n (d:ds) = if n `mod` d == 0
+                         then d
+                         else lowestDivisor n ds
+
+-- I don't understand where this algorithm came from but sampling some random, large
+-- values it seems to work correctly, so whatever.
 totient 1 = 1
-totient n | isPrime n = n - 1 
+totient n | isPrime n = n - 1
           | even n    = let m = n `div` 2
-                        in if odd m 
-                           then totient m
-                           else 2 * totient m
+                        in if odd m
+                          then totientMemo m
+                          else 2 * totientMemo m
           | otherwise = let m = lowestDivisor n . tail $ primes
                             n' = n `div` m
                             d = gcd m n'
-                        in ((totient n') * (totient m) * d) `div` totient d
-  where
-    lowestDivisor n (d:ds) = if n `mod` d == 0 
-                             then d
-                             else lowestDivisor n ds
+                        in ((totientMemo n') * (totientMemo m) * d) `div` totientMemo d
 
-main = print $ sum [x | x <- takeWhile (<40000000) primes, 
-                        (length . filter (==1) . take 25 . iterate totient $ x) == 1]
+totientMemo = memoize totient
+
+main = print $ [x | x <- takeWhile (<40000000) primes,
+                        (length . filter (==1) . take 25 . iterate totientMemo $ x) == 1]
