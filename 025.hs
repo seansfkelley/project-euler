794c84e0a9c68965c9f275f9fd579ef30e343c21
Sean Kelley <seansfkelley@gmail.com>
2017-09-10 14:42:00 -0700

    Solve 25.

--- 025.hs
+++ 025.hs
@@ -1,6 +1,5 @@
--- TODO: Force the usage of Integer rather than Int if it's not being used already.
--- TODO: Don't use logbase because it's not precise enough -- reimplement digit counter for Integer?
-findFib f0 f1 i = if logBase 10 f0 >= 1000
+findFib :: Integer -> Integer -> Int -> Int
+findFib f0 f1 i = if (length . show) f0 >= 1000
                   then i
                   else findFib f1 (f0 + f1) (i + 1)
 
