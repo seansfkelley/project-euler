7059968c576f6b4dceb7bdc8cfce81de5e0bb635
Sean Kelley <seansfkelley@gmail.com>
2017-09-11 14:17:48 -0700

    Clean up 37.

--- 037.hs
+++ 037.hs
@@ -1,12 +1,13 @@
 import Data.List
-import qualified Data.Numbers.Primes as Primes
+import Data.Numbers.Primes
 
 truncatable 2 = False
 truncatable 3 = False
 truncatable 5 = False
 truncatable 7 = False
-truncatable n = (trunc . init . tails . show $ n) && (trunc . tail . inits . show $ n)
+truncatable n = (trunc . init . tails $ shown) && (trunc . tail . inits $ shown)
   where
-    trunc = all Primes.isPrime . map read 
+    shown = show n
+    trunc = all isPrime . map read
 
-main = print $ sum $ take 11 $ filter truncatable Primes.primes
+main = print $ sum $ take 11 $ filter truncatable primes
