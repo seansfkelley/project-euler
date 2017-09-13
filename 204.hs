32449d21d1c3d5f9cac38247a76b1c8e7ff4d75c
Sean Kelley <seansfkelley@gmail.com>
2017-09-13 16:22:30 -0700

    Remove Rational from 204 because flooring division is equally correct.

--- 204.hs
+++ 204.hs
@@ -1,16 +1,16 @@
 import Data.Numbers.Primes (primes)
 
-integralLog :: Int -> Rational -> Int
-integralLog b n | n < (toRational b) = 0
-                | otherwise = 1 + integralLog b (n / (toRational b))
+integralLog :: Int -> Int -> Int
+integralLog b n | n < b = 0
+                | otherwise = 1 + integralLog b (n `div` b)
 
 hamming n lim =
   let
     hamming' n [] = 0
     hamming' n (p:ps) =
       let
-        fs = scanl (*) 1 $ replicate (integralLog p (toRational n)) p
-        recurse = (\f -> hamming' (n / (toRational f)) ps)
+        fs = scanl (*) 1 $ replicate (integralLog p n) p
+        recurse = (\f -> hamming' (n `div` f) ps)
       in
         (length fs) + (sum $ map recurse $ fs) - 1
   in
