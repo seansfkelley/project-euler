c0dfed1b79fccdb809d774d46281523c718bbbe1
Sean Kelley <seansfkelley@gmail.com>
2017-09-25 16:45:54 -0700

    Add a spec for a problem I came up with myself.

---
+++ own/efficient-enumeration.py
@@ -0,0 +1,7 @@
+# Generate the shortest bit string that contains every n-bit number from 0 to 2^n - 1 as a substring for any number n.
+#
+# Example for n = 2:
+#
+#   00110 or 10011
+#
+# These strings both contain each of the four 2-bit strings 00, 01, 10, 11 and are the shortest strings to do so (and may not be the only 5-character strings).
